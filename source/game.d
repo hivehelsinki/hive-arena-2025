d38e6e7198191743f39d7f3062f9b3347940bc19
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-07-03 12:18:02 +0300

    Spawing gamestate from map

---
+++ source/game.d
@@ -0,0 +1,45 @@
+import std.algorithm;
+
+import map;
+
+class GameState
+{
+	ubyte numPlayers;
+	Hex[Coords] hexes;
+	uint[] flowers;
+
+	static GameState spawn(const(Hex[Coords]) baseMap, ubyte numPlayers)
+	{
+		ubyte[] playerMapping;
+		assert(numPlayers <= 6);
+
+		switch(numPlayers)
+		{
+			case 1: playerMapping = [0, 1, 0, 0, 0, 0, 0]; break;
+			case 2: playerMapping = [0, 1, 0, 0, 2, 0, 0]; break;
+			case 3: playerMapping = [0, 1, 0, 2, 0, 3, 0]; break;
+			case 4: playerMapping = [0, 0, 1, 2, 0, 3, 4]; break;
+			case 5: playerMapping = [0, 1, 2, 3, 4, 5, 0]; break;
+			case 6: playerMapping = [0, 1, 2, 3, 4, 5, 6]; break;
+			default: throw new Exception("Invalid player count: " ~ numPlayers);
+		}
+
+		Hex[Coords] hexes;
+		foreach (coords, hex; baseMap)
+		{
+			Hex newHex = hex;
+			newHex.player = playerMapping[newHex.player];
+			if (newHex.kind.among(HexKind.HIVE, HexKind.BEE) && newHex.player == 0)
+				newHex.kind = HexKind.EMPTY;
+
+			hexes[coords] = newHex;
+		}
+
+		auto state = new GameState;
+		state.numPlayers = numPlayers;
+		state.hexes = hexes;
+		state.flowers = new uint[numPlayers + 1];
+
+		return state;
+	}
+}
--- source/main.d
+++ source/main.d
@@ -1,8 +1,12 @@
 import std.stdio;
+
 import map;
+import game;
 
 void main()
 {
 	auto map = loadMap("map.txt");
-	write(map.mapToString);
+	auto game = GameState.spawn(map, 3);
+
+	write(game.hexes.mapToString);
 }
