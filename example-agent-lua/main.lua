041c065cffead8d16f21358abdc6c58596678581
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-09-10 14:14:26 +0300

    Example agent does random moves

--- example-agent-lua/main.lua
+++ example-agent-lua/main.lua
@@ -11,11 +11,26 @@ if not host or not gameid or not name then
 	print "Usage: lua main.lua <host> <gameid> <name>"
 end
 
-local function callback(state, player)
+local function makeOrders(state, player)
 
 	utils.printState(state)
 
-	return {[0] = 0}
+	local orders = {}
+
+	for coords,hex in pairs(state.hexes) do
+		if hex.entity and hex.entity.type == "BEE" and hex.entity.player == player then
+
+			local order = {
+				type = "MOVE",
+				coords = coords,
+				direction = ({"NE", "E", "SE", "SW", "W", "NW"})[math.random(1,6)]
+			}
+
+			table.insert(orders, order)
+		end
+	end
+
+	return orders
 end
 
-arena.runAgent(host, gameid, name, callback)
+arena.runAgent(host, gameid, name, makeOrders)
---
+++ example-agent-lua/test.lua
@@ -0,0 +1,17 @@
+local http = require "http.request"
+local json = require "lunajson"
+
+local args = {...}
+local host = args[1]
+
+local headers, stream = http.new_from_uri(host .. "newgame?players=4&map=balanced"):go()
+local info = json.decode(stream:get_body_as_string())
+
+for i = 1,4 do
+	os.execute(string.format("lua main.lua %s %s %s %s",
+		host,
+		info.id,
+		"Team" .. i,
+		i == 4 and "" or " >> /dev/null &"
+	))
+end
