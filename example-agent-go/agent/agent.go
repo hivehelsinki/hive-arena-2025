2663b03f87e67b88293128cdece7524f09faf017
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-09-11 12:55:06 +0300

    Separated common game types

--- example-agent-go/agent/agent.go
+++ example-agent-go/agent/agent.go
@@ -8,9 +8,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
-	"strconv"
-	"errors"
 )
 
 func request(url string) string {
@@ -38,6 +35,11 @@ type JoinResponse struct {
 	Token string
 }
 
+type WebSocketMessage struct {
+	Turn     uint
+	GameOver bool
+}
+
 func joinGame(host string, id uint, name string) JoinResponse {
 
 	url := "http://" + host + fmt.Sprintf("/join?id=%d&name=%s", id, name)
@@ -49,11 +51,6 @@ func joinGame(host string, id uint, name string) JoinResponse {
 	fmt.Printf("Joined game %d as player %d\n", id, response.Id)
 
 	return response
-}
-
-type WebSocketMessage struct {
-	Turn     uint
-	GameOver bool
 }
 
 func startWebSocket(host string, id uint) *websocket.Conn {
@@ -70,56 +67,6 @@ func startWebSocket(host string, id uint) *websocket.Conn {
 	return ws
 }
 
-type Coords struct {
-	Row int
-	Col int
-}
-
-func (c Coords) MarshalText() (text []byte, err error) {
-	str := fmt.Sprintf("%d,%d", c.Row, c.Col)
-	return ([]byte)(str), nil
-}
-
-func (c *Coords) UnmarshalText(text []byte) error {
-	parts := strings.Split(string(text), ",")
-
-	if len(parts) != 2 {
-		return errors.New("Bad coords")
-	}
-
-	var err1, err2 error
-	c.Row, err1 = strconv.Atoi(parts[0])
-	c.Col, err2 = strconv.Atoi(parts[1])
-
-	if err1 != nil { return err1 }
-	if err2 != nil { return err2 }
-
-	return nil
-}
-
-type Entity struct {
-	Type   string
-	Hp     uint
-	Player uint
-}
-
-type Hex struct {
-	Terrain   string
-	Resources uint
-	Influence uint
-	Entity    *Entity
-}
-
-type GameState struct {
-	NumPlayers          uint
-	Turn                uint
-	Hexes               map[Coords]Hex
-	PlayerResources     []uint
-	lastInfluenceChange uint
-	Winners             map[uint]bool
-	GameOver            bool
-}
-
 func getState(host string, id uint, token string) GameState {
 
 	url := "http://" + host + fmt.Sprintf("/game?id=%d&token=%s", id, token)
@@ -129,12 +76,6 @@ func getState(host string, id uint, token string) GameState {
 	json.Unmarshal([]byte(body), &response)
 
 	return response
-}
-
-type Order struct {
-	Type string `json:"type"`
-	Coords Coords `json:"coords"`
-	Direction string `json:"direction"`
 }
 
 func sendOrders(host string, id uint, token string, orders []Order) {
