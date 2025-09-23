ab312db3f1175d76dfc9816f6c4a638b616bf81a
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-09-23 15:20:01 +0300

    Methodisized coords unmarshalling

--- common/serialization.go
+++ common/serialization.go
@@ -10,20 +10,22 @@ func (c Coords) String() string {
 	return fmt.Sprintf("%d,%d", c.Row, c.Col)
 }
 
-func CoordsFromString(s string) (Coords, error) {
+func (c *Coords) FromString(s string) error {
 	parts := strings.Split(s, ",")
 	if len(parts) != 2 {
-		return Coords{}, fmt.Errorf("invalid coordinate string format: %s", s)
+		return fmt.Errorf("invalid coordinate string format: %s", s)
 	}
 	row, err := strconv.Atoi(parts[0])
 	if err != nil {
-		return Coords{}, fmt.Errorf("invalid row value: %w", err)
+		return fmt.Errorf("invalid row value: %w", err)
 	}
 	col, err := strconv.Atoi(parts[1])
 	if err != nil {
-		return Coords{}, fmt.Errorf("invalid col value: %w", err)
+		return fmt.Errorf("invalid col value: %w", err)
 	}
-	return Coords{Row: row, Col: col}, nil
+	c.Row = row
+	c.Col = col
+	return nil
 }
 
 func (c Coords) MarshalText() (text []byte, err error) {
@@ -32,11 +34,9 @@ func (c Coords) MarshalText() (text []byte, err error) {
 
 func (c *Coords) UnmarshalText(b []byte) error {
 	str := string(b)
-	coords, err := CoordsFromString(str)
+	err := c.FromString(str)
 	if err != nil {
 		return err
 	}
-
-	*c = coords
 	return nil
 }
