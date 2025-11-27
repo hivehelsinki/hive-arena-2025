f10ddaba3b056c2e39941120dea6b5b7b021de66
Laurens Haas <lhaas@c1r5p21.hive.fi>
2025-11-27 12:29:37 +0200

    Add valid path lookki

--- strategrify-agent/commands.go
+++ strategrify-agent/commands.go
@@ -10,8 +10,8 @@ import (
 
 var dirs = []Direction{E, NE, SW, W, NW, NE}
 
-	// check GameState.NumPlayers & select strategy based on that
-func commands(state *GameState, player int,as *AgentState) []Order {
+// check GameState.NumPlayers & select strategy based on that
+func commands(state *GameState, player int, as *AgentState) []Order {
 	var orders []Order
 	// check GameState.NumPlayers & select strategy based on that AT SOME POINT LOL
 
@@ -51,13 +51,30 @@ func commands(state *GameState, player int,as *AgentState) []Order {
 	// EMILLIAS PATHFINDER WILL GO HERE
 			if foundHive {
 				if dir, ok := as.BestDirectionTowards(b.Coords, nearest); ok {
-					orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dir})
-					continue
+					if _, ok := IsValidMoveTarget(as, b.Coords, dir); ok {
+						orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dir})
+						continue
+					}
 				}
 			}
 
-			// fallback random move
-			orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dirs[rand.Intn(len(dirs))]})
+			maxTries := 10
+			var chosen Direction
+			found := false
+			for i := 0; i < maxTries; i++ {
+				d := dirs[rand.Intn(len(dirs))]
+				if _, ok := IsValidMoveTarget(as, b.Coords, d); ok {
+					chosen = d
+					found = true
+					break
+				}
+			}
+			if found {
+				orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: chosen})
+			} else {
+				orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dirs[rand.Intn(len(dirs))]})
+				//ATACK
+			}
 			continue
 		}
 
@@ -75,12 +92,35 @@ func commands(state *GameState, player int,as *AgentState) []Order {
 		if ok {
 	// EMILIAS PATHFINDER WILL GO HERE
 			if dir, ok2 := as.BestDirectionTowards(b.Coords, target); ok2 {
-				orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dir})
-				continue
+				if _, ok := IsValidMoveTarget(as, b.Coords, dir); ok {
+					orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dir})
+					continue
+				}
 			}
 		}
 		//maybe better move to oposite direction than hive
-		orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dirs[rand.Intn(len(dirs))]})
+		
+		
+		// fallback random move -- try a few times until we find a valid target
+		{
+			maxTries := 10
+			var chosen Direction
+			found := false
+			for i := 0; i < maxTries; i++ {
+				d := dirs[rand.Intn(len(dirs))]
+				if _, ok := IsValidMoveTarget(as, b.Coords, d); ok {
+					chosen = d
+					found = true
+					break
+				}
+			}
+			if found {
+				orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: chosen})
+			} else {
+				orders = append(orders, Order{Type: MOVE, Coords: b.Coords, Direction: dirs[rand.Intn(len(dirs))]})
+				//ATACK
+			}
+		}
 	}
 	return orders
 }
